7ef9670fd2b00ccaac915f16082215762eca67f3
Lawlzer <kevindaspam@gmail.com>
2023-04-08 15:51:30 -0500

    🎨Rename package to @lawlzer/misc Update dependencies/peerDependencies

--- config/lint-staged.config.js
+++ config/lint-staged.config.js
@@ -2,5 +2,12 @@
 // This will *only look at commited files* :)
 
 module.exports = {
-	'**/*.{ts,tsx,js,jsx,json,jsonc}': ['npm run lint:eslint:commit --', 'npm run lint:prettier --', 'jest --bail --findRelatedTests --pass-with-no-tests --config=./config/jest.config.ts'],
+	'**/*.{ts,tsx,js,jsx,json,jsonc}': [
+		//
+		'npm run lint:eslint:commit --',
+
+		'npm run lint:prettier --',
+		'jest --bail --findRelatedTests --pass-with-no-tests --config=./config/jest.config.ts',
+		'git add .',
+	],
 };
---
+++ packages/eslint/README.md
@@ -0,0 +1,12 @@
+npm link C:/code/npm/config/packages/eslint
+npm i @lawlzer/config
+npm i -D eslint
+
+const path = require('path');
+module.exports = {
+extends: [path.resolve(process.cwd(), 'node_modules', '@lawlzer', 'eslint', 'main', 'index.js')],
+};
+
+var example = 'hey! :)';
+
+Includes ESLint
